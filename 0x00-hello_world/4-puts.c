f34b5b713e023da2675cfd11ff0c56f8d7712cbc
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-02-17 11:47:50 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Program execution begins from here
+ *
+ * Return: 0 means (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
