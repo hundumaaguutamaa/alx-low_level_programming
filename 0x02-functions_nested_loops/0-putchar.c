104cd351f8a7997677fd20ec8109e8da8ec92312
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-02-21 12:21:34 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - program starts here
- * Return 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -15,5 +15,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return 0;
+	return (0);
 }
