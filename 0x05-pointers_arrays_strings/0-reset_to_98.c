52cf316690b546ce8af02a564992a0d071bf02c2
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-02-27 09:29:37 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Reset the value of the variable to 98
+ * @n: a pointer to the variable 
+ * Return: no return.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
