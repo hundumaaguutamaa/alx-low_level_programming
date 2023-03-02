4b729d14dd6a4f92ba60dce1dc467509e8bbb8fc
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-03-02 02:22:35 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,22 +10,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int a, b;
+	int i, j;
 
-	a = 0;
-	b = 0;
+	i = 0;
+	j = 0;
 
-	while (des [a] != '\0')
-		a++;
+	while (dest[i] != '\0')
+		i++;
 
-	while (src[b] != '\0' && b < n)
+	while (src[j] != '\0' && j < n)
 	{
-		dest[a] = src[b];
-		a++;
-		b++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 
-	dest[a] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
