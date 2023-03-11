01fcad6ba72da540bec00beca3f49970fc5f6f50
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-03-11 12:18:51 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,12 +5,11 @@
  * main - print the number of arguments passed
  * @argc: number of arguments
  * @argv: array of arguments
- *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc - 1);	
+	printf("%d\n", argc - 1);
 	return (0);
 }
 
