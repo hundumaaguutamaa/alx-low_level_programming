f74fccbe968976660c93e135b0bb8ef342bcbe0a
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-03-14 12:30:49 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,8 +2,7 @@
 #include "main.h"
 
 /**
- *
- * *create_array - creates an array of chars
+ * *create_array - creates an array of chars and initialize with specific character
  * @size: size of the array to create
  * @c: char to initialize the array c
  * Return: pointer to the array (Success), NULL (Error)
