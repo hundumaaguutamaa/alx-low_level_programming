524d5186cce7aa4e629eb3e970d9af54e512b77d
hundumaaguutamaa <hundumaa.guutamaa>
2023-06-15 17:00:04 -0700

    0-main.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,17 +6,25 @@
  * 
  * Return: The number of nodes.
  */
- 
-size_t print_dlistint(const dlistint_t *h)
+    size_t print_dlistint(const dlistint_t *h)
 {
- int count = 0;
+	int numof_node;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        count++;
-        h = h->next;
-    }
-    return (count);
+	numof_node = 0;
+
+	if (h == NULL)
+		return (numof_node);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		numof_node++;
+		h = h->next;
+	}
+
+	return (numof_node);
 }
 
