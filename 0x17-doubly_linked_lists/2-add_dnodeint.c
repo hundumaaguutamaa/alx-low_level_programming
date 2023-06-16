dacba81836a847015b004cf13ba41468fb2c0117
hundumaaguutamaa <hundumaa.guutamaa>
2023-06-15 17:37:01 -0700

    2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+  * add_dnodeint - Adds a node to the beginning of a list.
+  * @head: head of linked list.
+  * @n: value to add to list.
+  *
+  * Return: pointer to the element.
+  */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+{
+
+	dlistint_t *temp;
+
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->prev = NULL;
+	node->next = NULL;
+
+	if (*head == NULL)
+	{
+		node->next = NULL;
+		*head = node;
+		
+        return (*head);
+	}
+	temp = *head;
+	node->next = temp;
+	temp->prev = node;
+	*head = node;
+
+	return (node);
+}
+
