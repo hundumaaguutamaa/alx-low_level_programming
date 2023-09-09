2cbced42299c9f1b7903ba80506a3a92180ae2fc
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-09-09 13:34:50 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,1 +1,27 @@
 #include "hash_tables.h"
+
+/**
+ * hash_table_create - function to create the hash table
+ * @size: size of an array
+ *
+ * Return: pointer of new created hash table
+ */ 
+
+hash_table_t *hash_table_create(unsigned long int size);
+{
+	hash_table_t *hash_t;
+
+	if (size == 0)
+		return (NULL);
+
+	hash_t = malloc(sizeof(hash_table_t)):
+	if (!hash_t)
+		return(NULL);
+
+	hash_t->size = size;
+	hash_t->array = calloc((size_t)size, sizeof(hash_node_t *));
+
+	if (hash_t->array == NULL)
+		return (NULL);
+	return (hash_t);
+}
