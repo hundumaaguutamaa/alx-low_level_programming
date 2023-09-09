7fe7ea043b492cd40462f69e13a84a6cb3aa55e2
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-09-09 23:52:43 +0300

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size);
 	hash_table_t *hash_t;
 
 	if (size == 0)
-        return (NULL);
+        	return (NULL);
 
     hash_t = malloc(sizeof(hash_table_t));
     if (hash_t == NULL)
