2ca2c5a6d28066d2f1ae59e31bb63c66acf69182
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-09-13 12:09:58 +0300

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -2,14 +2,14 @@
 
 /**
  * hash_table_delete - Deletes a hash table
- * @ht: Hash table to be deleted
+ * @ht: The hash table to be deleted
  *
  */
 
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-    hash_node_t *p_node;
+    hash_node_t *node, *node;
 	hash_table_t *head = ht;
 	
 
@@ -17,14 +17,14 @@ void hash_table_delete(hash_table_t *ht)
 	{
 		if (ht->array[i] != NULL)
 		{
-			p_node = ht->array[i];
-			while (p_node != NULL)
+			node = ht->array[i];
+			while (node != NULL)
 			{
-				temp = p_node->next;
-				free(p_node->key);
-				free(p_node->value);
-				free(p_node);
-				p_node = temp;
+				temp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = temp;
 			}
 		}
 	}
